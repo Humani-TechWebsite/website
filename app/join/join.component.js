db4be2e5f82960ac070936f3c14bbc2ab1a08e3d
Deb Banerji <debkbanerji@gmail.com>
2016-11-27 15:53:53 -0500

    Back to Doing Actual Work

--- app/join/join.component.js
+++ app/join/join.component.js
@@ -1,7 +1,25 @@
 angular.module('join').component('join', {
     templateUrl: 'join/join.template.html',
 
-    controller: [function joinController() {
+    controller: ['$firebaseObject', function joinController($firebaseObject) {
         var self = this;
+        self.email = "";
+        self.showError = false;
+        self.joinRequestsRef = firebase.database().ref().child("join-requests");
+        self.gatechRegExp = new RegExp("(.+)@gatech.edu");
+        // console.log(self.gatechRegExp.test("dbanerji3@gatech.edu"));
+        self.joinRequest = function () {
+            if (self.gatechRegExp.test(self.email)) {
+                var time = Date.now();
+                var requestRef = self.joinRequestsRef.child(time);
+                var requestObj = $firebaseObject(requestRef);
+                requestObj.$value = self.email;
+                requestObj.$save();
+                self.email = "";
+                self.showError = false;
+            } else {
+                self.showError = true;
+            }
+        }
     }]
 });
